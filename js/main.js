3372f735c7f276a6322219127cc33061c50f81da
eashankumaaar00005-sudo <eashankumaaar00005@gmail.com>
2026-08-01 16:57:30 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,48 @@
-const btn=document.getElementById("openLetter");
+const tl = gsap.timeline();
 
-btn.onclick=()=>{
+tl.to(".bar", {
+
+width: "100%",
+
+duration: 2.2,
+
+ease: "power2.inOut"
+
+})
+
+.to("#loadingText", {
+
+text: "Ready",
+
+duration: .3
+
+})
+
+.to("#loader", {
+
+opacity:0,
+
+duration:1
+
+})
+
+.set("#loader",{
+
+display:"none"
+
+})
+
+.to(".landing",{
+
+opacity:1,
+
+duration:1
+
+});
+
+document
+.getElementById("start")
+.onclick=()=>{
 
 window.location="letter.html";
 
